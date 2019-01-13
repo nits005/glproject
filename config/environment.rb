c232dd670e50192c133f4ca4e2cebb9fc837220d
Nits PC <nitinrathod005@gmail.com>
2019-01-13 23:55:49 +0530

    Production env fix

--- config/environment.rb
+++ config/environment.rb
@@ -1,5 +1,8 @@
 # Load the Rails application.
 require_relative 'application'
 
+ENV['RAILS_SERVE_STATIC_FILES'] = '1'
+ENV["RAILS_LOG_TO_STDOUT"] = 'true'
+
 # Initialize the Rails application.
 Rails.application.initialize!
